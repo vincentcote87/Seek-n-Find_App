3607526e7c5d384c966992482476a89991843c4d
Vincent <vincentcote87@gmail.com>
2019-03-12 13:00:39 -0600

    Cordova not loading properly

--- Seek-n-Find/platforms/browser/www/js/app.js
+++ Seek-n-Find/platforms/browser/www/js/app.js
@@ -6,43 +6,24 @@ function init() {
   var cameraOptions = {
     quality: 100,
     correctOrientation: true,
-    saveToPhotoAlbum: true,
+    saveToPhotoAlbum: false,
     allowEdit: true,
   };
 
-  $('#cameraBtn').on('click', function () {
-    navigator.camera.getPicture(cameraSuccess, cameraFail, cameraOptions);
+  $(document).on('click', '.item', () => {
+    console.log($(this).attr('class'));
   });
 
-  $('#btn').on('click', function () {
-    $("#content").append("clicked");
-    navigator.geolocation.watchPosition(geoSuccess, geoError);
-  });
-
-  function cameraSuccess(imageURI) {
-    console.log('You got a photo');
-    $('#cameraBtn').after(`<img src="${imageURI}">`);
-  }
-
-  function cameraFail(msg) {
-    console.log(msg);
-  }
-
-  var geoSuccess = function (position) {
-    $("#content").append('Latitude: ' + position.coords.latitude + '<br>' +
-      'Longitude: ' + position.coords.longitude + '<br>' +
-      'Altitude: ' + position.coords.altitude + '<br>' +
-      'Accuracy: ' + position.coords.accuracy + '<br>' +
-      'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br>' +
-      'Heading: ' + position.coords.heading + '<br>' +
-      'Speed: ' + position.coords.speed + '<br>' +
-      'Timestamp: ' + position.timestamp + '<br><br>');
-  };
-
-  // onError Callback receives a PositionError object
-  //
-  function geoError(error) {
-    alert('code: ' + error.code + '\n' +
-      'message: ' + error.message + '\n');
-  }
 };
+
+$('#startBtn').on('click', () => {
+  window.location.href = './setup.html';
+});
+
+$('#casualBtn').on('click', () => {
+  window.location.href = './gamePage.html';
+});
+
+$('#timeAttackBtn').on('click', () => {
+  window.location.href = './gamePage.html';
+});
